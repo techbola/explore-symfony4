ecd2296e6340ffdc2557bbea58812ab9b4d932e7
Adebola <bfagbowore@gmail.com>
2022-01-22 18:09:32 +0100

    send contact form message via symfony mailer

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,17 +4,46 @@ namespace App\Controller;
 
 use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class ContactController extends AbstractController
 {
     /**
      * @Route("/contact", name="contact")
      */
-    public function contact(): Response
+    public function contact(Request $request, MailerInterface $mailer): Response
     {
         $form = $this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+
+        $this->addFlash('info', 'some info here');
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contactFormData = $form->getData();
+
+            $email = (new Email())
+                ->from($contactFormData['email'])
+                ->to('hello@testsymfony.com')
+                //->cc('cc@example.com')
+                //->bcc('bcc@example.com')
+                //->replyTo('fabien@example.com')
+                //->priority(Email::PRIORITY_HIGH)
+                ->subject('Test Symfony Mailer!')
+                ->text($contactFormData['message'])
+                ->html($contactFormData['message']);
+
+            $mailer->send($email);
+
+            $this->addFlash('success', 'message was sent');
+
+            return $this->redirectToRoute('contact');
+        }
+
         return $this->render('contact/index.html.twig', [
             'our_form' => $form->createView(),
         ]);
